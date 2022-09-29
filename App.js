fd676a645c227354978899aa5ac601c0eb6fce23
SantiagoGaray32 <santi.garay2811@gmail.com>
2022-09-28 22:56:43 -0300

    creando mobile app

--- App.js
+++ App.js
@@ -1,10 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ScrollView>
+
+      <Text style={styles.text}>La Copa Libertadores de América (Taça Libertadores da América en portugués), denominada oficialmente Copa Conmebol Libertadores,4​ y llamada simplemente Copa Libertadores (Taça Libertadores en portugués); es un torneo anual internacional oficial de fútbol organizado por la Confederación Sudamericana de Fútbol, creado en 1960 bajo la denominación de Copa de Campeones de América (Copa dos Campeões da América en portugués) o Copa Campeones de América. En 1965 el torneo adoptó el nombre «Libertadores» en honor a los líderes de las guerras de independencia hispanoamericanas y brasileña de América del Sur.5​ Es uno de los más prestigiosos torneos en el mundo y la más prestigiosa competición a nivel de clubes de fútbol en Sudamérica.
+
+El torneo ha tenido diferentes formatos desde su creación. Al inicio, solo participaban los campeones de las ligas. En 1966, comenzaron a participar los subcampeones.5​ En 1998, los equipos de México fueron invitados a competir y participaron de forma regular hasta el 2016, en el 2017 se retiraron del torneo por no lograr ajustarse al nuevo calendario aprobado para los años siguientes.6​ En el año 2000 el torneo se expandió de 20 a 32 equipos.
+
+Al menos cuatro clubes por país compiten en el torneo, mientras que Argentina y Brasil tienen la mayor cantidad de clubes participantes. Tradicionalmente, se ha utilizado siempre una fase de grupos, pero el número de equipos por grupo ha variado en numerosas ocasiones. En la actualidad, desde la edición 2017, participan 47 clubes y se disputa anualmente desde enero hasta noviembre.7​8​
+
+Desde 1989 el ganador de esta competición disputa la Recopa Sudamericana y, desde 2005, la Copa Mundial de Clubes, competición —esta última— que reemplazó a la Copa Intercontinental, la cual se jugó de 1960 a 2004 entre el ganador del torneo y el campeón de Europa. También, clasifica a la segunda fase de la siguiente Copa Libertadores. Asimismo, entre 1969 y 1998 el campeón disputaba la Copa Interamericana con el vencedor de la Copa de Campeones de la Concacaf. Y también desde 1993 hasta 1996, el campeón jugó la Copa de Oro con el ganador de la Supercopa Sudamericana, el campeón de la Copa Conmebol, y el campeón de la Copa Máster de Supercopa (luego reemplazada por la Copa Máster de Conmebol). Entre 1998 y 2016, los clubes de México participaron de la competición continental con notables actuaciones; entre ellas, tres finales disputadas —aunque no pudieron ganarla en ninguna (2001, 2010 y 2015)—.
+
+El club con más títulos es Independiente, de Argentina, con siete conquistas. El actual campeón del certamen es el equipo brasileño Palmeiras, que logró su tercer título, segundo de forma consecutiva. La Copa Libertadores de América (Taça Libertadores da América en portugués), denominada oficialmente Copa Conmebol Libertadores,4​ y llamada simplemente Copa Libertadores (Taça Libertadores en portugués); es un torneo anual internacional oficial de fútbol organizado por la Confederación Sudamericana de Fútbol, creado en 1960 bajo la denominación de Copa de Campeones de América (Copa dos Campeões da América en portugués) o Copa Campeones de América. En 1965 el torneo adoptó el nombre «Libertadores» en honor a los líderes de las guerras de independencia hispanoamericanas y brasileña de América del Sur.5​ Es uno de los más prestigiosos torneos en el mundo y la más prestigiosa competición a nivel de clubes de fútbol en Sudamérica.
+
+El torneo ha tenido diferentes formatos desde su creación. Al inicio, solo participaban los campeones de las ligas. En 1966, comenzaron a participar los subcampeones.5​ En 1998, los equipos de México fueron invitados a competir y participaron de forma regular hasta el 2016, en el 2017 se retiraron del torneo por no lograr ajustarse al nuevo calendario aprobado para los años siguientes.6​ En el año 2000 el torneo se expandió de 20 a 32 equipos.
+
+Al menos cuatro clubes por país compiten en el torneo, mientras que Argentina y Brasil tienen la mayor cantidad de clubes participantes. Tradicionalmente, se ha utilizado siempre una fase de grupos, pero el número de equipos por grupo ha variado en numerosas ocasiones. En la actualidad, desde la edición 2017, participan 47 clubes y se disputa anualmente desde enero hasta noviembre.7​8​
+
+Desde 1989 el ganador de esta competición disputa la Recopa Sudamericana y, desde 2005, la Copa Mundial de Clubes, competición —esta última— que reemplazó a la Copa Intercontinental, la cual se jugó de 1960 a 2004 entre el ganador del torneo y el campeón de Europa. También, clasifica a la segunda fase de la siguiente Copa Libertadores. Asimismo, entre 1969 y 1998 el campeón disputaba la Copa Interamericana con el vencedor de la Copa de Campeones de la Concacaf. Y también desde 1993 hasta 1996, el campeón jugó la Copa de Oro con el ganador de la Supercopa Sudamericana, el campeón de la Copa Conmebol, y el campeón de la Copa Máster de Supercopa (luego reemplazada por la Copa Máster de Conmebol). Entre 1998 y 2016, los clubes de México participaron de la competición continental con notables actuaciones; entre ellas, tres finales disputadas —aunque no pudieron ganarla en ninguna (2001, 2010 y 2015)—.
+
+El club con más títulos es Independiente, de Argentina, con siete conquistas. El actual campeón del certamen es el equipo brasileño Palmeiras, que logró su tercer título, segundo de forma consecutiva. La Copa Libertadores de América (Taça Libertadores da América en portugués), denominada oficialmente Copa Conmebol Libertadores,4​ y llamada simplemente Copa Libertadores (Taça Libertadores en portugués); es un torneo anual internacional oficial de fútbol organizado por la Confederación Sudamericana de Fútbol, creado en 1960 bajo la denominación de Copa de Campeones de América (Copa dos Campeões da América en portugués) o Copa Campeones de América. En 1965 el torneo adoptó el nombre «Libertadores» en honor a los líderes de las guerras de independencia hispanoamericanas y brasileña de América del Sur.5​ Es uno de los más prestigiosos torneos en el mundo y la más prestigiosa competición a nivel de clubes de fútbol en Sudamérica.
+
+El torneo ha tenido diferentes formatos desde su creación. Al inicio, solo participaban los campeones de las ligas. En 1966, comenzaron a participar los subcampeones.5​ En 1998, los equipos de México fueron invitados a competir y participaron de forma regular hasta el 2016, en el 2017 se retiraron del torneo por no lograr ajustarse al nuevo calendario aprobado para los años siguientes.6​ En el año 2000 el torneo se expandió de 20 a 32 equipos.
+
+Al menos cuatro clubes por país compiten en el torneo, mientras que Argentina y Brasil tienen la mayor cantidad de clubes participantes. Tradicionalmente, se ha utilizado siempre una fase de grupos, pero el número de equipos por grupo ha variado en numerosas ocasiones. En la actualidad, desde la edición 2017, participan 47 clubes y se disputa anualmente desde enero hasta noviembre.7​8​
+
+Desde 1989 el ganador de esta competición disputa la Recopa Sudamericana y, desde 2005, la Copa Mundial de Clubes, competición —esta última— que reemplazó a la Copa Intercontinental, la cual se jugó de 1960 a 2004 entre el ganador del torneo y el campeón de Europa. También, clasifica a la segunda fase de la siguiente Copa Libertadores. Asimismo, entre 1969 y 1998 el campeón disputaba la Copa Interamericana con el vencedor de la Copa de Campeones de la Concacaf. Y también desde 1993 hasta 1996, el campeón jugó la Copa de Oro con el ganador de la Supercopa Sudamericana, el campeón de la Copa Conmebol, y el campeón de la Copa Máster de Supercopa (luego reemplazada por la Copa Máster de Conmebol). Entre 1998 y 2016, los clubes de México participaron de la competición continental con notables actuaciones; entre ellas, tres finales disputadas —aunque no pudieron ganarla en ninguna (2001, 2010 y 2015)—.
+
+El club con más títulos es Independiente, de Argentina, con siete conquistas. El actual campeón del certamen es el equipo brasileño Palmeiras, que logró su tercer título, segundo de forma consecutiva. La Copa Libertadores de América (Taça Libertadores da América en portugués), denominada oficialmente Copa Conmebol Libertadores,4​ y llamada simplemente Copa Libertadores (Taça Libertadores en portugués); es un torneo anual internacional oficial de fútbol organizado por la Confederación Sudamericana de Fútbol, creado en 1960 bajo la denominación de Copa de Campeones de América (Copa dos Campeões da América en portugués) o Copa Campeones de América. En 1965 el torneo adoptó el nombre «Libertadores» en honor a los líderes de las guerras de independencia hispanoamericanas y brasileña de América del Sur.5​ Es uno de los más prestigiosos torneos en el mundo y la más prestigiosa competición a nivel de clubes de fútbol en Sudamérica.
+
+El torneo ha tenido diferentes formatos desde su creación. Al inicio, solo participaban los campeones de las ligas. En 1966, comenzaron a participar los subcampeones.5​ En 1998, los equipos de México fueron invitados a competir y participaron de forma regular hasta el 2016, en el 2017 se retiraron del torneo por no lograr ajustarse al nuevo calendario aprobado para los años siguientes.6​ En el año 2000 el torneo se expandió de 20 a 32 equipos.
+
+Al menos cuatro clubes por país compiten en el torneo, mientras que Argentina y Brasil tienen la mayor cantidad de clubes participantes. Tradicionalmente, se ha utilizado siempre una fase de grupos, pero el número de equipos por grupo ha variado en numerosas ocasiones. En la actualidad, desde la edición 2017, participan 47 clubes y se disputa anualmente desde enero hasta noviembre.7​8​
+
+Desde 1989 el ganador de esta competición disputa la Recopa Sudamericana y, desde 2005, la Copa Mundial de Clubes, competición —esta última— que reemplazó a la Copa Intercontinental, la cual se jugó de 1960 a 2004 entre el ganador del torneo y el campeón de Europa. También, clasifica a la segunda fase de la siguiente Copa Libertadores. Asimismo, entre 1969 y 1998 el campeón disputaba la Copa Interamericana con el vencedor de la Copa de Campeones de la Concacaf. Y también desde 1993 hasta 1996, el campeón jugó la Copa de Oro con el ganador de la Supercopa Sudamericana, el campeón de la Copa Conmebol, y el campeón de la Copa Máster de Supercopa (luego reemplazada por la Copa Máster de Conmebol). Entre 1998 y 2016, los clubes de México participaron de la competición continental con notables actuaciones; entre ellas, tres finales disputadas —aunque no pudieron ganarla en ninguna (2001, 2010 y 2015)—.
+
+El club con más títulos es Independiente, de Argentina, con siete conquistas. El actual campeón del certamen es el equipo brasileño Palmeiras, que logró su tercer título, segundo de forma consecutiva.  </Text>
+      <Image style={styles.tinyLogo} source={{uri: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.ole.com.ar%2F2021%2F11%2F22%2Fe7mPpjGEK_2000x1500__1.jpg&imgrefurl=https%3A%2F%2Fwww.ole.com.ar%2Ffutbol-internacional%2Flibertadores%2Fmapa-libertadores-2022-_0_KASjUxGo0.html&tbnid=YO2r7T0_OKzPkM&vet=12ahUKEwiUsreF27j6AhX9u5UCHVIHD44QMygPegUIARD5AQ..i&docid=GUyxJvAHG3dCGM&w=2000&h=1500&q=copa%20libertadores&ved=2ahUKEwiUsreF27j6AhX9u5UCHVIHD44QMygPegUIARD5AQ'}} />
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +53,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'blue'
+  },
+  tinyLogo:{
+    width:50,
+    heigth:50
+  }
 });
